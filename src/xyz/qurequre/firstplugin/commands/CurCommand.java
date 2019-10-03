c3de0c9955aca983920698cc251863dc3945beb0
jwkim101201 <jwkim101201@gmail.com>
2019-10-04 06:03:59 +0900

    Commit

--- src/xyz/qurequre/firstplugin/commands/CurCommand.java
+++ src/xyz/qurequre/firstplugin/commands/CurCommand.java
@@ -2,16 +2,17 @@ package xyz.qurequre.firstplugin.commands;
 
 import xyz.qurequre.firstplugin.Main;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class HelloCommand implements CommandExecutor {
+public class CurCommand implements CommandExecutor {
 	private Main plugin;
 	
-	public HelloCommand(Main plugin) {
+	public CurCommand(Main plugin) {
 		this.plugin = plugin;
-		plugin.getCommand("hello").setExecutor(this);
+		plugin.getCommand("cur").setExecutor(this);
 	}
 	
 	@Override
@@ -19,11 +20,12 @@ public class HelloCommand implements CommandExecutor {
 		if(!(sender instanceof Player)) {
 			sender.sendMessage(sender.getName() + ", only players can excute this command!");
 			return true;
+		} else {
+			Player pl = (Player) sender;
+			GameMode gm = pl.getGameMode();
+			
+			pl.sendMessage(pl.getName() + ", Your Gamemode is: " + gm);
+			return true;
 		}
-		
-		Player p = (Player) sender;
-		
-		p.sendMessage(p.getName() + ", ¾È³ç");
-		return true;
 	}
 }
